d728a5b9b478e1d174c0fc595bea5d0c70a65baa
Alexander Rutz <ar@animaux.de>
2016-03-10 16:44:00 +0100

    Now works with multiple and no textareas
    - Now works with multiple markdown
    - No more JS Error when there’s no textarea on the page

--- assets/simplemde.publish.js
+++ assets/simplemde.publish.js
@@ -2,28 +2,29 @@
 	'use strict';
 
 	$(window).on('load', function() {
-
-    var simplemde = new SimpleMDE({
-      // for options see: https://github.com/NextStepWebs/simplemde-markdown-editor/
-      element: $('textarea[class*="markdown"], textarea[class*="commonmark"]')[0],
-      spellChecker: false,
-      status: false,
-      indentWithTabs: false,
-      toolbar: ["heading-1",
-                "heading-2",
-                "heading-3",
-                "|",
-                "bold",
-                "italic",
-                "|",
-                "link",
-                "|",
-                "unordered-list",
-                "ordered-list",
-                "|",
-                "horizontal-rule",]
+	  $('textarea[class*="markdown"], textarea[class*="commonmark"]').each(function(i) {
+	    	    
+      new SimpleMDE({
+        // for options see: https://github.com/NextStepWebs/simplemde-markdown-editor/
+        element: $(this)[0],
+        spellChecker: false,
+        status: false,
+        indentWithTabs: false,
+        toolbar: ["heading-1",
+                  "heading-2",
+                  "heading-3",
+                  "|",
+                  "bold",
+                  "italic",
+                  "|",
+                  "link",
+                  "|",
+                  "unordered-list",
+                  "ordered-list",
+                  "|",
+                  "horizontal-rule",]
+      });
     });
-
 	});
 
 })(window.jQuery, window.Symphony);
